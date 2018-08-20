533badafcfe20eb0d4413bbf60111c179e05df51
YXSui <yuxuansui@outlook.com>
2018-08-20 16:39:07 +0800

    update

--- dash5.py
+++ dash5.py
@@ -8,11 +8,16 @@ from plotly import figure_factory as ff
 import json
 import pandas as pd
 import numpy as np
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 function=['function_1','clean_data','function_3','function_4']
 
 app = dash.Dash()
 df = pd.read_csv(
-    'train.csv')
+    'train1.csv')
+table=df.copy()
 cols = df.columns
 app.layout=html.Div([
     html.H1([
@@ -29,7 +34,7 @@ app.layout=html.Div([
     html.Div([
         dcc.Dropdown(id='xialaicaidan1',
         options=[
-            {'label': 'clean_data', 'value': 'clean'},
+            {'label': 'change SalePrice data', 'value': 'clean'},
             {'label': 'function_1', 'value': '1'},
             {'label': 'function_2', 'value': '2'},
             {'label': 'function_3', 'value': '3'}
@@ -41,16 +46,16 @@ app.layout=html.Div([
         html.Table([html.Tr([html.Th(col) for col in df.columns])] +
            [html.Tr([
                html.Td(df.iloc[i][col]) for col in df.columns
-           ]) for i in range(min(len(df),5))]
+           ]) for i in range(min(len(df),100))]
        )],
-            style={'display':'inline-block','height':'400px','width':'98%','overflow':'scroll','font-size':'20px','margin':'20px','backgroundColor':'white'}
+            style={'display':'inline-block','height':'400px','width':'98%','overflow':'scroll','font-size':'20px','margin':'20px','backgroundColor':'black','textAlign': 'center','color':'white'}
             ),
     html.Div([
         html.Div([
             dcc.Dropdown(
                 id='crossfilter-xaxis-column',
                 options=[{'label': i, 'value': i}for i in cols],
-                value='YearBuilt'
+                value='Neighborhood'
             ),
             dcc.RadioItems(
                 id='crossfilter-xaxis-type',
@@ -89,7 +94,11 @@ app.layout=html.Div([
                 'width':'95%',
                 'height':'30%',
                 'margin':'10px',
-                'backgroundColor':'black'
+                'backgroundColor':'black',
+                'plot_bgcolor':colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font':{
+                    'color': colors['text']}
         }
         ),
 
@@ -99,48 +108,68 @@ app.layout=html.Div([
             'position':'inline-block',
             'width':'98%',
             'height':'30%',
-            'margin':'10px',
-            'backgroundColor':'black'
+            'margin':'0px',
+            'backgroundColor':'black',
+            'plot_bgcolor':colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font':{
+                    'color': colors['text']}
         }
         ),
     dcc.Graph(
         id='graph3',
         style={
-            'float':'left',
-            'width':'45%',
-            'height':'20%',
-            'margin':'10px',
-            'backgroundColor':'black'
+        'float':'right',
+            'width':'49%',
+            'height':'25%',
+            'margin':'0px',
+            'backgroundColor':'black',
+                'plot_bgcolor':colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font':{
+                    'color': colors['text']}
         }
         ),
     dcc.Graph(
         id='graph4',
         style={
-        'float':'left',
-            'width':'45%',
-            'height':'20%',
-            'margin':'10px',
-            'backgroundColor':'black'
+        'position':'inline-block',
+            'width':'49%',
+            'height':'25%',
+            'margin':'0px',
+            'backgroundColor':'black',
+                'plot_bgcolor':colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font':{
+                    'color': colors['text']}
         }
-        ),
-    dcc.Graph(
+        ), dcc.Graph(
         id='graph5',
         style={
-        'float':'left',
-            'width':'45%',
-            'height':'20%',
-            'margin':'10px',
-            'backgroundColor':'black'}
+        'float':'right',
+            'width':'49%',
+            'height':'25%',
+            'margin':'0px',
+            'backgroundColor':'black',
+                'plot_bgcolor':colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font':{
+                    'color': colors['text']}}
         ),
-    dcc.Graph(
+        dcc.Graph(
         id='graph6',
-        style={
-        'float':'left',
-            'width':'45%',
-            'height':'20%',
-            'margin':'10px',
-            'backgroundColor':'black'}
-        )
+        style={'position':'inline-block',
+            'width':'49%',
+            'height':'30%',
+            'margin':'0px',
+            'backgroundColor':'black',
+                'plot_bgcolor':colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font':{
+                    'color': colors['text']}}
+        ),
+    
+   
 ],style={'backgroundColor':'black'})
     ],style={'backgroundColor':'black'})
 
@@ -152,62 +181,62 @@ app.layout=html.Div([
 def update_output_div(input_value):
     return 'You choose to show "{}" rows'.format(input_value)
 
+
 @app.callback(
     dash.dependencies.Output('graph1','figure'),
-    [dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
+    [dash.dependencies.Input('xialaicaidan1', 'value'),
+    dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
      dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
      dash.dependencies.Input('crossfilter-xaxis-type', 'value'),
-     dash.dependencies.Input('crossfilter-yaxis-type', 'value'),
-     dash.dependencies.Input('xialaicaidan1','value')]
-     )
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 xaxis_type, yaxis_type,value):
-    
-    if len(df[xaxis_column_name].unique())<=20:
-        data_1={}
+     dash.dependencies.Input('crossfilter-yaxis-type', 'value')
+     ]
+     )    
+def update_graph(value,xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type):
+    if 'clean' in value:
+        for i in range(0,len(df)):
+            df['SalePrice'][i]=10000
+        print('SalePrice has been changed')
+    if len(df[xaxis_column_name].unique())<=26:
+        type_list=[]
         for each in df[xaxis_column_name].unique():
-            data_1[each]=0
-        for each in df[xaxis_column_name]:
-            data_1[each]=data_1[each]+1
-        print(data_1)
-        for key in data_1:
-            print(key)
-        x0=[213,314,2343,25,345,341,41,341,22]
-        return{
-        'data':[go.Box(
-            y=x0
-            ),
-        go.Box(y=x0)]
-        }
-    else:
-        return {
-            'data': [go.Scatter(
-                x=df[xaxis_column_name],
-                y=df[yaxis_column_name],
-                text=df[yaxis_column_name],
-                customdata=df[yaxis_column_name],
-                mode='markers',
-                marker={
-                    'size': 10,
-                    'opacity': 0.7,
-                    'line': {'width': 0.5, 'color': 'white'}
-                }
-            )
-            ],
-            'layout': go.Layout(      
-                xaxis={
-                    'title': xaxis_column_name,
-                    'type': 'linear' if xaxis_type == 'Linear' else 'log'
-                },
-                yaxis={
-                    'title': yaxis_column_name,
-                    'type': 'linear' if yaxis_type == 'Linear' else 'log'
-                },
-                margin={'l': 40, 'b': 30, 't': 10, 'r': 0},
-                height=450,
-                hovermode='closest'
-            )
+            type_list.append(each)
+        for j in range(0,len(df)):
+            for i in range(0,len(type_list)):
+                if df[xaxis_column_name][j]==type_list[i]:
+                    df[xaxis_column_name][j]=i
+    return {
+        'data': [go.Scatter(
+            x=df[xaxis_column_name],
+            y=df[yaxis_column_name],
+            text=df[yaxis_column_name],
+            customdata=df[yaxis_column_name],
+            mode='markers',
+            marker={
+                'size': 10,
+                'opacity': 0.7,
+                'line': {'width': 0.5, 'color': 'white'}
             }
+        )
+        ],
+        'layout': go.Layout(      
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear' if xaxis_type == 'Linear' else 'log'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear' if yaxis_type == 'Linear' else 'log'
+            },
+            margin={'l': 70, 'b': 70, 't': 10, 'r': 0},
+            height=450,
+            hovermode='closest',
+            plot_bgcolor= colors['background'],
+            paper_bgcolor= colors['background'],
+            font={
+                'color': colors['text']
+        
+        })}
 
 @app.callback(
     dash.dependencies.Output('graph2','figure'),
@@ -219,27 +248,19 @@ def update_graph_graph2(xaxis_column_name, yaxis_column_name
     dic={}
     num={}
     avg=[]
-    for i in range(0,len(df)-1):
-        dic[df[xaxis_column_name][i]]=0
-    for i in range(0,len(df)-1):
-        num[df[xaxis_column_name][i]]=0
-    for i in range(0,len(df)-1):
-        num[df[xaxis_column_name][i]]=1+num[df[xaxis_column_name][i]]
-    for i in range(0,len(df)-1):
-        dic[df[xaxis_column_name][i]]=df[yaxis_column_name][i]+dic[df[xaxis_column_name][i]]
+    for i in range(0,len(table)-1):
+        dic[table[xaxis_column_name][i]]=0
+    for i in range(0,len(table)-1):
+        num[table[xaxis_column_name][i]]=0
+    for i in range(0,len(table)-1):
+        num[table[xaxis_column_name][i]]=1+num[table[xaxis_column_name][i]]
+    for i in range(0,len(table)-1):
+        dic[table[xaxis_column_name][i]]=table[yaxis_column_name][i]+dic[table[xaxis_column_name][i]]
     for key in num:
         avg.append(dic[key]/num[key])
-    x1 = np.random.randn(200) - 2 
-    x2 = np.random.randn(200)
-    x3 = np.random.randn(200) + 2 
-
-    hist_data = [x1, x2, x3]
-
-    group_labels = ['Group 1', 'Group 2', 'Group 3']
-    colors = ['#A56CC1', '#A6ACEC', '#63F5EF']
     return{
     'data': [go.Bar(
-            x=sorted(df[xaxis_column_name].unique()),
+            x=sorted(table[xaxis_column_name].unique()),
             y=avg,
             marker={
                 'line': {'width': 0}},
@@ -252,19 +273,25 @@ def update_graph_graph2(xaxis_column_name, yaxis_column_name
             yaxis={
                 'title': yaxis_column_name
             },
-            margin={'l': 40, 'b': 30, 't': 40, 'r': 0},
+            margin={'l': 70, 'b': 70, 't': 40, 'r': 0},
             height=450,
             hovermode='closest',
-            title='The avearge of each data'
-        )
-    }
+            title='The avearge of each data',
+             plot_bgcolor= colors['background'],
+                paper_bgcolor= colors['background'],
+                font={
+                    'color': colors['text']
+        
+    })}
 @app.callback(
     dash.dependencies.Output('graph3','figure'),
     [dash.dependencies.Input('crossfilter-xaxis-column','value'),
-    dash.dependencies.Input('crossfilter-yaxis-column','value')
+    dash.dependencies.Input('crossfilter-yaxis-column','value'),
+    dash.dependencies.Input('xialaicaidan1','value')
     ])
-def update_graph_graph3(xaxis_column_name, yaxis_column_name):
-    labels=sorted(df[xaxis_column_name])
+def update_graph_graph3(xaxis_column_name, yaxis_column_name,value):
+    
+    labels=sorted(table[xaxis_column_name])
     counts = {}
     times=[]
     each_1=[]
@@ -276,13 +303,13 @@ def update_graph_graph3(xaxis_column_name, yaxis_column_name):
     for each in counts:
         each_1.append(each)
         times.append(counts[each])
-    if len(df[xaxis_column_name].unique())<=20:
+    if len(table[xaxis_column_name].unique())<=26:
         return {
         'data':[go.Pie(
             labels=each_1,
             values=times
             )],
-        'layout': go.Layout(       #da tu shu ju
+        'layout': go.Layout(      
                 xaxis={
                     'title': xaxis_column_name
                 },
@@ -290,17 +317,21 @@ def update_graph_graph3(xaxis_column_name, yaxis_column_name):
                     'title': yaxis_column_name
                 },
                 title='The number of each data',
-                margin={'l': 40, 'b': 30, 't': 50, 'r': 20},
                 height=450,
-                hovermode='closest'
-            )
-        }
+                hovermode='closest',
+                margin={'l': 70, 'b': 70, 't': 40, 'r': 0},
+                plot_bgcolor= colors['background'],
+                paper_bgcolor= colors['background'],
+                font={
+                    'color': colors['text']
+            }
+        )}
     else:
         return {'data':[go.Pie(
             labels=['NAN'],
             values=[1]
             )],
-        'layout': go.Layout(       #da tu shu ju
+        'layout': go.Layout(
                 xaxis={
                     'title': xaxis_column_name
                 },
@@ -308,21 +339,26 @@ def update_graph_graph3(xaxis_column_name, yaxis_column_name):
                     'title': yaxis_column_name
                 },
                 title='The number of each data',
-                margin={'l': 40, 'b': 30, 't': 50, 'r': 20},
+            margin={'l': 70, 'b': 70, 't': 40, 'r': 0},
                 height=450,
-                hovermode='closest'
-            )}
+                hovermode='closest',
+                plot_bgcolor= colors['background'],
+                paper_bgcolor= colors['background'],
+                font={
+                    'color': colors['text']
+        })}
 
 @app.callback(
     dash.dependencies.Output('graph4','figure'),
     [dash.dependencies.Input('crossfilter-xaxis-column','value'),
     dash.dependencies.Input('crossfilter-yaxis-column','value')
     ])
-def update_graph_graph3(xaxis_column_name, yaxis_column_name):
-    if len(df[xaxis_column_name].unique())<=20:
+def update_graph_graph4(xaxis_column_name, yaxis_column_name):
+    
+    if len(table[xaxis_column_name].unique())<=26:
         return {
         'data':[go.Histogram(
-            x=sorted(df[xaxis_column_name]),
+            x=sorted(table[xaxis_column_name]),
             marker={
                 'line': {'width': 0}}
             )],
@@ -336,7 +372,12 @@ def update_graph_graph3(xaxis_column_name, yaxis_column_name):
                 title='The number of each data',
                 margin={'l': 40, 'b': 30, 't': 50, 'r': 20},
                 height=450,
-                hovermode='closest'
+                hovermode='closest',
+                  plot_bgcolor= colors['background'],
+                paper_bgcolor= colors['background'],
+                font={
+                    'color': colors['text']
+        }
             )
         }
     else:
@@ -353,15 +394,20 @@ def update_graph_graph3(xaxis_column_name, yaxis_column_name):
                 title='The number of each data',
                 margin={'l': 40, 'b': 30, 't': 50, 'r': 20},
                 height=450,
-                hovermode='closest'
+                hovermode='closest',
+                  plot_bgcolor= colors['background'],
+                paper_bgcolor= colors['background'],
+                font={
+                    'color': colors['text']
+        }
             )}
 @app.callback(
     dash.dependencies.Output('graph5','figure'),
     [dash.dependencies.Input('crossfilter-xaxis-column','value'),
     dash.dependencies.Input('crossfilter-yaxis-column','value')
     ])
-def update_graph_graph3(xaxis_column_name, yaxis_column_name):
-    labels=sorted(df[xaxis_column_name])
+def update_graph_graph5(xaxis_column_name, yaxis_column_name):
+    labels=sorted(table[xaxis_column_name])
     counts = {}
     times=[]
     each_1=[]
@@ -374,8 +420,8 @@ def update_graph_graph3(xaxis_column_name, yaxis_column_name):
     for each in counts:
         each_1.append(each)
         times.append(counts[each])
-    if len(df[xaxis_column_name].unique())<=20:
-        a=sorted(df[xaxis_column_name].unique())
+    if len(table[xaxis_column_name].unique())<=26:
+        a=sorted(table[xaxis_column_name].unique())
         b=times
         a.append(a[0])
         b.append(b[0])
@@ -397,7 +443,11 @@ def update_graph_graph3(xaxis_column_name, yaxis_column_name):
                   range = [min1, max1]
                 )
               ),
-              showlegend = True
+              showlegend = True,
+              plot_bgcolor= colors['background'],
+                paper_bgcolor= colors['background'],
+                font={
+                    'color': colors['text']}
             ),
         }
     else:
@@ -415,7 +465,11 @@ def update_graph_graph3(xaxis_column_name, yaxis_column_name):
                 title='The number of each data',
                 margin={'l': 40, 'b': 30, 't': 50, 'r': 20},
                 height=450,
-                hovermode='closest'
+                hovermode='closest',
+                plot_bgcolor= colors['background'],
+                paper_bgcolor= colors['background'],
+                font={
+                    'color': colors['text']}
             )}
 @app.callback(
     dash.dependencies.Output('graph6','figure'),
@@ -423,45 +477,47 @@ def update_graph_graph3(xaxis_column_name, yaxis_column_name):
     dash.dependencies.Input('crossfilter-yaxis-column','value')
     ])
 def update_graph_graph6(xaxis_column_name, yaxis_column_name):
-    if len(df[xaxis_column_name].unique())>=20:
-        return {
-        'data':[
-            go.Histogram2dContour(
-                x = df[xaxis_column_name],
-                y = df[yaxis_column_name],
-                colorscale = 'Blues',
-                reversescale = True,
-                xaxis = 'x',
-                yaxis = 'y'
-            ),
-        ],
+    return {
+    'data':[
+        go.Histogram2dContour(
+            x = table[xaxis_column_name],
+            y = table[yaxis_column_name],
+            colorscale = 'Blues',
+            reversescale = True,
+            xaxis = 'x',
+            yaxis = 'y'
+        ),
+    ],
 
-        'layout':go.Layout(
-            autosize = False,
-            xaxis = dict(
-                zeroline = False,
-                showgrid = False
-            ),
-            yaxis = dict(
-                zeroline = False,
-                
-                showgrid = False
-            ),
-            xaxis2 = dict(
-        zeroline = False,
-      
-        showgrid = False
-    ),
-    yaxis2 = dict(
-        zeroline = False,
+    'layout':go.Layout(
+        autosize = False,
+        xaxis = dict(
+            zeroline = False,
+            showgrid = False
+        ),
+        yaxis = dict(
+            zeroline = False,
+            
+            showgrid = False
+        ),
+        xaxis2 = dict(
+    zeroline = False,
+  
+    showgrid = False
+),
+yaxis2 = dict(
+    zeroline = False,
 
-        showgrid = False
-    ),
-            height = 600,
-            width = 600,
-            bargap = 0,
-            hovermode = 'closest',
-            showlegend = False
-        )}
+    showgrid = False
+),
+        hovermode = 'closest',
+        showlegend = True,
+          plot_bgcolor= colors['background'],
+            paper_bgcolor= colors['background'],
+        margin={'l': 70, 'b': 70, 't': 40, 'r': 0},
+            font={
+                'color': colors['text']
+    }
+    )}
 if __name__ == '__main__':
     app.run_server(debug=True)
